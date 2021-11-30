3c33af6f141e10d165e06df4a7254e3b9a128dcd
ELVINMAMMADLI <mamedli.elvin@gmail.com>
2021-11-30 18:43:58 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+let page = 1;
 
 function App() {
+  const [userId, setUserId] = useState(0);
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = (type) => {
+    axios
+      .get(`https://reqres.in/api/users?page=${page}`)
+      .then((result) => result.data)
+      .then((data) => data.data)
+      .then((data) => {
+        if (data.length > 0) {
+          setUsers(data);
+          if (type === "next") {
+            setUserId(0);
+          } else if (type === "prev") {
+            setUserId(data.length - 1);
+          }
+        } else {
+          if (type === "next") {
+            page -= 1;
+          } else if (type === "prev") {
+            page += 1;
+          }
+        }
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  const PrevUserClickHandler = () => {
+    if (users[userId - 1]) {
+      setUserId(userId - 1);
+    } else {
+      if (page - 1 > 0) {
+        page -= 1;
+        getUsers("prev");
+      }
+    }
+  };
+
+  const NextUserClickHandler = () => {
+    if (users[userId + 1]) {
+      setUserId(userId + 1);
+    } else {
+      page += 1;
+      getUsers("next");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {users ? (
+        <div className="App">
+          <img className="image" src={users[userId].avatar} alt="" />
+          <h1 className="fullName">
+            {users[userId].first_name + " " + users[userId].last_name}
+          </h1>
+          <p className="email">{users[userId].email}</p>
+          <button className="prevUser" onClick={PrevUserClickHandler}>
+            Prev User
+          </button>
+          <button className="nextUser" onClick={NextUserClickHandler}>
+            Next User
+          </button>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
   );
 }
 
